2f428abd661f78b50adf314a1ad9ce6bbcc67e8b
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2018-01-05 01:18:41 +0000

    sha1 fnks

--- src/clj_kad/core.clj
+++ src/clj_kad/core.clj
@@ -1,5 +1,22 @@
 (ns clj-kad.core)
 
+(defn get-hash [type data]
+  (.digest (java.security.MessageDigest/getInstance type) (.getBytes data) ))
+
+(defn sha1-hash [data]
+  (get-hash "sha1" data))
+
+(defn get-hash-str [data-bytes]
+  (apply str
+         (map
+           #(.substring
+              (Integer/toString
+                (+ (bit-and % 0xff) 0x100) 16) 1)
+           data-bytes)
+         ))
+
+(def key-bits-length 160)
+
 (defn foo
   "I don't do a whole lot."
   [x]
