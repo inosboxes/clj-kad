e6a8f19967510261e8a818a81cdc5bb896f81289
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2018-01-05 00:59:39 +0000

    init

---
+++ src/clj_kad/core.clj
@@ -0,0 +1,6 @@
+(ns clj-kad.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_kad/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-kad.core-test
+  (:require [clojure.test :refer :all]
+            [clj-kad.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
