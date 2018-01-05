743a4bec06101c842ebd95227b70c796eb2a3e12
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2018-01-05 21:29:31 +0000

    * Durable cross-platform key-value store protocol with core.async. * adding cljfmt to plugins for usage as lein task

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject clj-kad "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :plugins [[lein-cljfmt "0.5.7"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.4.4"]
+                 [io.replikativ/konserve "0.4.11"]
                  [lein-cljfmt "0.5.7"]])
