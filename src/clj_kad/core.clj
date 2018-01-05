3b18c4965cb53338e4580aae2acc5278c6da1561
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2018-01-05 20:55:00 +0000

    structuring abstraction using defprotocol and defrecord for now, kadtools used as "visual" reference"

--- src/clj_kad/core.clj
+++ src/clj_kad/core.clj
@@ -23,3 +23,60 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defprotocol AbstractNetworkNodeP
+  "Abstract Node"
+  (listen [this] )
+  (plugin [this plugin])
+  (receive-message [this request response])
+  (send-message [this method params contact callback])
+  (use-middleware [this method middleware]))
+
+(defprotocol KademliaNodeP
+  "Kademlia Node"
+  (join [this seed]))
+
+(defrecord NodeInstance
+  [transport storage logger messenger node-identity]
+  AbstractNetworkNodeP
+  KademliaNodeP
+  (listen [this] (println "start listening"))
+  (join [this seed] (println seed)))
+
+(defprotocol IdentityP
+  "Kademlia Identity"
+  (get-identity [this])
+  (get-host [this])
+  (get-port [this]))
+
+(defrecord NodeIdentityInstance
+  [id host port]
+  IdentityP
+  (get-identity [this] id)
+  (get-host [this] host)
+  (get-port [this] port))
+
+
+(defprotocol Bucket)
+(defprotocol KademliaRule)
+(defprotocol Transport)
+(defprotocol Messenger)
+(defprotocol Logger)
+(defprotocol RoutingTable)
+(defprotocol ErrorRule)
+
+(defn -main
+  "Main function"
+  []
+  (let  [
+         seed-node-identity  (NodeIdentityInstance. 1 "localhost" 1337)
+         node-identity  (NodeIdentityInstance. nil "localhost" 1338)
+         transport nil
+         storage nil
+         logger nil
+         messenger nil
+         kadSeedNode  (NodeInstance. transport storage logger messenger node-identity)
+         ]
+    (listen kadSeedNode)
+    )
+  )
